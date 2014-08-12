5bb09313ccd00a30250cce028f436fa243cd343b
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-12 19:30:38 +0300

    io

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ app.set('port', 3000);
 var server = require('http').createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
 });
+var io = require('socket.io')(server);
 
 app.use('/api', require('./api/router'));
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function (socket) {
+    console.log('a user connected', socket.id/*, socket.request.headers*/);
+    socket.on('disconnect', function(){
+        console.log('user disconnected', socket.id);
+    });
+
+    socket.on('app_started', function(socket){
+        console.log('app started', socket.id);
+    });
+});
 
 module.exports = app;
