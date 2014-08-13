91659b423a3a634a149a70574cd207f0ae592351
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-13 13:58:00 +0300

    migration

--- migrations/20140813115024_init.js
+++ migrations/20140813115024_init.js
@@ -2,14 +2,86 @@
 
 exports.up = function (knex, Promise) {
     return Promise.all([
+        knex.schema.createTable('image', function (table) {
+            table.increments('id');
+            table.string('title');
+            table.text('description');
+            table.string('file');
+            table.boolean('is_active')
+                .defaultTo(true)
+                .notNullable();
+            table.boolean('is_banned')
+                .defaultTo(false)
+                .notNullable();
+            table.timestamps();
+        }),
         knex.schema.createTable('user', function (table) {
             table.increments('id');
+            table.integer('image_id')
+                .references('id')
+                .inTable('image')
+                .onDelete('SET NULL')
+                .onUpdate('CASCADE');
             table.string('first_name');
             table.string('last_name');
+            table.string('email')
+                .notNullable();
+            table.string('password', 40);
+            table.timestamp('last_login');
+            table.boolean('is_active')
+                .defaultTo(true)
+                .notNullable();
+            table.boolean('is_banned')
+                .defaultTo(false)
+                .notNullable();
             table.timestamps();
         }),
         knex.schema.createTable('shop', function (table) {
             table.increments('id');
+            table.integer('image_id')
+                .references('id')
+                .inTable('image')
+                .onDelete('SET NULL')
+                .onUpdate('CASCADE');
+            table.string('title')
+                .notNullable();
+            table.text('description');
+            table.string('location');
+            table.float('tax', 4, 2)
+                .defaultTo(0)
+                .notNullable();
+            table.float('rating', 3, 2);
+            table.boolean('is_active')
+                .defaultTo(true)
+                .notNullable();
+            table.boolean('is_banned')
+                .defaultTo(false)
+                .notNullable();
+            table.timestamps();
+        }),
+        knex.schema.createTable('manager', function (table) {
+            table.increments('id');
+            table.integer('user_id')
+                .references('id')
+                .inTable('user')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            table.integer('shop_id')
+                .references('id')
+                .inTable('shop')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            table.enu('role', ['editor','seller','owner'])
+                .notNullable();
+            table.boolean('is_public')
+                .defaultTo(true)
+                .notNullable();
+            table.boolean('is_active')
+                .defaultTo(true)
+                .notNullable();
+            table.boolean('is_banned')
+                .defaultTo(false)
+                .notNullable();
             table.timestamps();
         })
     ]);
@@ -17,7 +89,9 @@ exports.up = function (knex, Promise) {
 
 exports.down = function (knex, Promise) {
     return Promise.all([
+        knex.schema.dropTable('manager'),
+        knex.schema.dropTable('shop'),
         knex.schema.dropTable('user'),
-        knex.schema.dropTable('shop')
+        knex.schema.dropTable('image')
     ]);
 };
