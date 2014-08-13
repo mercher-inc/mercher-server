b5d2ab2c10d709f80d63c20f73941402ce83d023
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-13 15:48:56 +0300

    api shop and user items

--- routes/api/v1/shops.js
+++ routes/api/v1/shops.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var ShopsCollection = require('../../../collections/shops');
+var ShopModel = require('../../../models/shop');
 
 router.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     next();
 });
 
-/* GET home page. */
 router.get('/', function (req, res) {
     var shopsCollection = new ShopsCollection();
     shopsCollection
@@ -15,6 +15,25 @@ router.get('/', function (req, res) {
         .then(function (collection) {
             res.json(collection);
         });
+});
+
+router.param('shopId', function (req, res, next, id) {
+    var shopModel = new ShopModel({id: id});
+    shopModel.fetch({require: true})
+        .then(function (model) {
+            req.shop = model;
+            next();
+        })
+        .catch(ShopModel.NotFoundError, function () {
+            res.status(404).json(null);
+        })
+        .catch(function (err) {
+            next(err);
+        });
+});
+
+router.get('/:shopId', function (req, res) {
+    res.json(req.shop);
 });
 
 module.exports = router;
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var UsersCollection = require('../../../collections/users');
+var UserModel = require('../../../models/user');
 
 router.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     next();
 });
 
-/* GET home page. */
 router.get('/', function (req, res) {
     var usersCollection = new UsersCollection();
     usersCollection
@@ -15,6 +15,25 @@ router.get('/', function (req, res) {
         .then(function (collection) {
             res.json(collection);
         });
+});
+
+router.param('userId', function (req, res, next, id) {
+    var userModel = new UserModel({id: id});
+    userModel.fetch({require: true})
+        .then(function (model) {
+            req.user = model;
+            next();
+        })
+        .catch(UserModel.NotFoundError, function () {
+            res.status(404).json(null);
+        })
+        .catch(function (err) {
+            next(err);
+        });
+});
+
+router.get('/:userId', function (req, res) {
+    res.json(req.user);
 });
 
 module.exports = router;
