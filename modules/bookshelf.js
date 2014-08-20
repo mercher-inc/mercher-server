51a8bce88e2b694229f0f360b5b2eeb434ca050a
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-20 17:46:08 +0300

    bookshelf config

--- modules/bookshelf.js
+++ modules/bookshelf.js
@@ -1,13 +1,5 @@
-var knex = require('knex')({
-    client:     'pg',
-    connection: {
-        host:     '127.0.0.1',
-        user:     'postgres',
-        password: 'postgres',
-        database: 'mercher_v2',
-        charset:  'utf8'
-    }
-});
-var bookshelf = require('bookshelf')(knex);
+var knexfile = require('../knexfile'),
+    knex = require('knex')(knexfile.development),
+    bookshelf = require('bookshelf')(knex);
 
 module.exports = bookshelf;
