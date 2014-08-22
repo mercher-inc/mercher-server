b494f7f166021e730887d9e4e9431d6d1ac071ee
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-22 20:04:00 +0300

    basic auth logic moved to user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,62 @@
-var bookshelf = require('../modules/bookshelf');
+var bookshelf = require('../modules/bookshelf'),
+    Promise = require('bluebird'),
+    crypto = require('crypto'),
+    expressAsyncValidator = require('express-async-validator'),
+    salt = 'Mercher';
 
-var Model = bookshelf.Model.extend({
-    tableName: 'user',
-    hasTimestamps: true
-});
+var UserModel = bookshelf.Model.extend(
+    {
+        tableName:     'user',
+        hasTimestamps: true
+    },
+    {
+        login: function (email, password) {
+            var UserModel = this;
+            return new Promise(function (resolve, reject) {
+                new (expressAsyncValidator.model)(
+                    {
+                        "email":    {
+                            "rules":      {
+                                "required": {
+                                    "message": "Email is required"
+                                },
+                                "isEmail":  {
+                                    "message": "Email should be a valid email address"
+                                }
+                            },
+                            "allowEmpty": false
+                        },
+                        "password": {
+                            "rules":      {
+                                "required": {
+                                    "message": "Password is required"
+                                }
+                            },
+                            "allowEmpty": false
+                        }
+                    }
+                )
+                    .validate({email: email, password: password})
+                    .then(function (model) {
+                        new UserModel()
+                            .where({
+                                email:    model.email,
+                                password: crypto.pbkdf2Sync(model.password, salt, 10, 20).toString('hex')
+                            })
+                            .fetch({require: true})
+                            .then(function (userModel) {
+                                resolve(userModel);
+                            })
+                            .catch(UserModel.NotFoundError, function (error) {
+                                reject(error);
+                            });
+                    })
+                    .catch(expressAsyncValidator.errors.modelValidationError, function (error) {
+                        reject(error);
+                    });
+            });
+        }
+    }
+);
 
-module.exports = Model;
+module.exports = UserModel;
--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -3,6 +3,7 @@ var express = require('express'),
     _ = require('underscore'),
     router = express.Router(),
     UserModel = require('../../../models/user'),
+    expressAsyncValidator = require('express-async-validator'),
     AccessTokenModel = require('../../../models/access_token');
 
 router.use('/sign_up', function (req, res, next) {
@@ -118,62 +119,14 @@ router.post('/sign_up', function (req, res, next) {
         });
 });
 
-router.use('/basic', function (req, res, next) {
-    req
-        .model({
-            "email":    {
-                "rules":      {
-                    "required": {
-                        "message": "Email is required"
-                    },
-                    "isEmail":  {
-                        "message": "Email should be a valid email address"
-                    }
-                },
-                "source":     ["body"],
-                "allowEmpty": false
-            },
-            "password": {
-                "rules":      {
-                    "required": {
-                        "message": "Password is required"
-                    },
-                    "isLength": {
-                        "message": "Password should be between 8 and 40 characters long",
-                        "min":     8,
-                        "max":     40
-                    }
-                },
-                "source":     ["body"],
-                "allowEmpty": false
-            }
-        })
-        .validate()
-        .then(function () {
-            next();
-        })
-        .catch(function (error) {
-            var validationError = new (require('./errors/validation'))("Validation failed", error);
-            next(validationError);
-        });
-});
-
 router.post('/basic', function (req, res, next) {
     res.set({
         'Access-Control-Allow-Methods': 'POST'
     });
     res.removeHeader('Access-Control-Allow-Origin');
 
-    var userModel = new UserModel();
-
-    userModel
-        .where({
-            email:    req.body.email,
-            password: crypto.pbkdf2Sync(req.body.password, 'Mercher', 10, 20).toString('hex')
-        })
-        .fetch({require: true})
+    UserModel.login(req.body.email, req.body.password)
         .then(function (userModel) {
-
             var hash = crypto.createHash('sha1');
             hash.update(Math.random().toString(), 'utf8');
             hash.update(new Date().toString(), 'utf8');
@@ -188,6 +141,7 @@ router.post('/basic', function (req, res, next) {
                 token:   token,
                 expires: expires
             });
+
             accessTokenModel
                 .save().then(function (accessTokenModel) {
                     res.status(201).json({
@@ -199,6 +153,10 @@ router.post('/basic', function (req, res, next) {
         .catch(UserModel.NotFoundError, function () {
             var notFoundError = new (require('./errors/not_found'))("User with these credentials was not found");
             next(notFoundError);
+        })
+        .catch(expressAsyncValidator.errors.modelValidationError, function (error) {
+            var validationError = new (require('./errors/validation'))("Validation failed", error);
+            next(validationError);
         });
 });
 
