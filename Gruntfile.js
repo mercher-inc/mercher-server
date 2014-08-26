c3ff61f8db9250529d28df86df6515094d6b0cf5
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-26 19:55:01 +0300

    grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            'dist'
+                        ]
+                    }
+                ]
+            }
+        },
+
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd:    '',
+                        dest:   'dist',
+                        src:    [
+                            'collections/*.js',
+                            'models/*.js',
+                            'modules/**/*.js',
+                            'routes/**/*.js',
+                            'app.js',
+                            'package.json'
+                        ]
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'copy:dist'
+    ]);
+};
