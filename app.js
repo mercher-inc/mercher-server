9545ab0a9d107547bee0b95d6a4c3022fc3f653e
Dmitry Les <dmitriy.s.les@gmail.com>
2014-08-27 14:13:18 +0300

    env

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('port', process.env.PORT || 3000);
 
 var server = require('http').createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
+    console.log(process.env);
 });
 var io = require('socket.io')(server);
 
