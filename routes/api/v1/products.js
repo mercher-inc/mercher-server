8e34dff8df31ce6bcbd742538510c120805a52b6
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-01 13:25:04 +0300

    Get product

--- routes/api/v1/products.js
+++ routes/api/v1/products.js
@@ -200,8 +200,53 @@ router.post('/', function (req, res, next) {
             var validationError = new (require('./errors/validation'))("Validation failed", error);
             next(validationError);
         });
+});
 
+router.param('productId', function (req, res, next) {
+    req
+        .model({
+            "productId": {
+                "rules":      {
+                    "required":  {
+                        "message": "Product ID is required"
+                    },
+                    "isNumeric": {
+                        "message": "Product ID should be numeric"
+                    },
+                    "toInt":     {}
+                },
+                "source":     ["params"],
+                "allowEmpty": false
+            }
+        })
+        .validate()
+        .then(function () {
+            var productModel = new ProductModel({id: req.params.productId});
+            productModel.fetch({require: true})
+                .then(function (model) {
+                    req.product = model;
+                    next();
+                })
+                .catch(ProductModel.NotFoundError, function () {
+                    var notFoundError = new (require('./errors/not_found'))("Product was not found");
+                    next(notFoundError);
+                })
+                .catch(function (err) {
+                    next(err);
+                });
+        })
+        .catch(function (error) {
+            var badRequestError = new (require('./errors/bad_request'))("Bad request", error);
+            next(badRequestError);
+        });
+});
 
+router.get('/:productId', function (req, res) {
+    req.product
+        .load('shop.image')
+        .then(function () {
+            res.json(req.product);
+        });
 });
 
 module.exports = router;
