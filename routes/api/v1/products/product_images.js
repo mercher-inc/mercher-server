fe28f57b7a47b090dbe3b349eee8e21266263570
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-02 14:53:03 +0300

    product images list

--- routes/api/v1/products/product_images.js
+++ routes/api/v1/products/product_images.js
@@ -1,10 +1,55 @@
 var express = require('express'),
     router = express.Router(),
+    Promise = require("bluebird"),
+    bookshelf = require('../../../../modules/bookshelf'),
     busboy = require('connect-busboy'),
     ImageModel = require('../../../../models/image'),
+    ProductImagesCollection = require('../../../../collections/product_images'),
     ProductImageModel = require('../../../../models/product_image');
 
-router.use('/', busboy());
+router.use('/', function (req, res, next) {
+    res.set({
+        'Access-Control-Allow-Methods': 'GET,POST'
+    });
+    next();
+});
+
+router.get('/', require('../middleware/collection_params_check'));
+
+router.get('/', function (req, res, next) {
+    var productImagesCollection = new ProductImagesCollection();
+    var productImageModel = new ProductImageModel();
+
+    var collectionRequest = productImagesCollection
+        .query(function (qb) {
+            qb.where('product_id', '=', req.product.id).limit(req.query.limit).offset(req.query.offset);
+        })
+        .fetch({
+            withRelated: ['image']
+        });
+
+    var totalRequest = bookshelf
+        .knex(productImageModel.tableName)
+        .where('product_id', '=', req.product.id)
+        .count(productImageModel.idAttribute)
+        .then(function (result) {
+            return parseInt(result[0].count);
+        });
+
+    Promise
+        .props({
+            product_images: collectionRequest,
+            total:          totalRequest
+        })
+        .then(function (results) {
+            res.json(results);
+        })
+        .catch(function (err) {
+            next(err);
+        });
+});
+
+router.post('/', busboy());
 
 router.post('/', require('../middleware/auth_check'));
 
