83eac2344d0ef8173343890a7595a56afebbd0a9
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-12 16:21:03 +0300

    relations fix

--- models/product_image.js
+++ models/product_image.js
@@ -1,18 +1,16 @@
 var app = require('../app'),
     io = app.get('io'),
-    BaseModel = require('./base'),
-    ImageModel = require('./image'),
-    ProductModel = require('./product');
+    BaseModel = require('./base');
 
 var ProductImageModel = BaseModel.extend(
     {
         tableName: 'product_image',
 
         image:   function () {
-            return this.belongsTo(ImageModel);
+            return this.belongsTo(require('./image'));
         },
         product: function () {
-            return this.belongsTo(ProductModel);
+            return this.belongsTo(require('./product'));
         }
     }
 );
