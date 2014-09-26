b393eb19948767e0078a0a4c7ec1f1388845558d
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-26 16:32:53 +0300

    M2-39 Add new fields to migration

--- migrations/20140926142156_product_additional_fields.js
+++ migrations/20140926142156_product_additional_fields.js
@@ -3,7 +3,7 @@
 exports.up = function (knex, Promise) {
     return knex.transaction(function (trx) {
         return Promise.all([
-            trx.schema.raw('CREATE TYPE "product_availability" AS ENUM (\'preorder\', \'inStock\', \'availableForOrder\', \'outOfStock\')'),
+            trx.schema.raw('CREATE TYPE "product_availability" AS ENUM (\'inStock\', \'availableForOrder\', \'outOfStock\')'),
             trx.schema.raw('CREATE TYPE "product_age_group" AS ENUM (\'kids\', \'adult\')'),
             trx.schema.raw('CREATE TYPE "product_condition" AS ENUM (\'new\', \'refurbished\', \'used\')'),
             trx.schema.raw('CREATE TYPE "product_target_gender" AS ENUM (\'female\', \'male\', \'unisex\')')
@@ -18,13 +18,18 @@ exports.up = function (knex, Promise) {
                         .defaultTo('new')
                         .notNullable();
                     table.specificType('target_gender', 'product_target_gender');
+                    table.timestamp('expiration_time');
+
                     table.string('brand');
                     table.string('manufacturer_part_number');
+                    table.string('ean');
+                    table.string('isbn');
+                    table.string('upc');
                     table.string('color');
                     table.string('material');
                     table.string('pattern');
                     table.string('size');
-                    table.timestamp('expiration_time');
+                    table.string('weight');
                 });
         });
     });
@@ -38,13 +43,17 @@ exports.down = function (knex, Promise) {
                 table.dropColumn('age_group');
                 table.dropColumn('condition');
                 table.dropColumn('target_gender');
+                table.dropColumn('expiration_time');
                 table.dropColumn('brand');
                 table.dropColumn('manufacturer_part_number');
+                table.dropColumn('ean');
+                table.dropColumn('isbn');
+                table.dropColumn('upc');
                 table.dropColumn('color');
                 table.dropColumn('material');
                 table.dropColumn('pattern');
                 table.dropColumn('size');
-                table.dropColumn('expiration_time');
+                table.dropColumn('weight');
             })
             .then(function () {
                 return Promise.all([
