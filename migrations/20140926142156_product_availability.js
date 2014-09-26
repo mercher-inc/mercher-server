fff5f956f8de6ba167c42eca364ecbe87e2fd3f0
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-26 14:56:29 +0300

    M2-39 Add new fields to migration

--- migrations/20140926142156_product_availability.js
+++ migrations/20140926142156_product_availability.js
@@ -1,28 +1,58 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
     return knex.transaction(function (trx) {
-        return trx.schema
-            .raw('CREATE TYPE "product_availability" AS ENUM (\'preorder\', \'inStock\', \'availableForOrder\', \'outOfStock\')')
-            .then(function(){
-                return trx.schema
-                    .table('product', function (table) {
-                        table.specificType('availability', 'product_availability')
-                            .defaultTo('inStock')
-                            .notNullable();
-                    });
-            });
+        return Promise.all([
+            trx.schema.raw('CREATE TYPE "product_availability" AS ENUM (\'preorder\', \'inStock\', \'availableForOrder\', \'outOfStock\')'),
+            trx.schema.raw('CREATE TYPE "product_age_group" AS ENUM (\'kids\', \'adult\')'),
+            trx.schema.raw('CREATE TYPE "product_condition" AS ENUM (\'new\', \'refurbished\', \'used\')'),
+            trx.schema.raw('CREATE TYPE "product_target_gender" AS ENUM (\'female\', \'male\', \'unisex\')')
+        ]).then(function () {
+            return trx.schema
+                .table('product', function (table) {
+                    table.specificType('availability', 'product_availability')
+                        .defaultTo('inStock')
+                        .notNullable();
+                    table.specificType('age_group', 'product_age_group');
+                    table.specificType('condition', 'product_condition')
+                        .defaultTo('new')
+                        .notNullable();
+                    table.specificType('target_gender', 'product_target_gender');
+                    table.string('brand');
+                    table.string('manufacturer_part_number');
+                    table.string('color');
+                    table.string('material');
+                    table.string('pattern');
+                    table.string('size');
+                    table.timestamp('expiration_time');
+                });
+        });
     });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
     return knex.transaction(function (trx) {
         return trx.schema
             .table('product', function (table) {
-                table.dropColumn('availability')
+                table.dropColumn('availability');
+                table.dropColumn('age_group');
+                table.dropColumn('condition');
+                table.dropColumn('target_gender');
+                table.dropColumn('brand');
+                table.dropColumn('manufacturer_part_number');
+                table.dropColumn('color');
+                table.dropColumn('material');
+                table.dropColumn('pattern');
+                table.dropColumn('size');
+                table.dropColumn('expiration_time');
             })
-            .then(function(){
-                return trx.schema.raw('DROP TYPE "product_availability"');
+            .then(function () {
+                return Promise.all([
+                    trx.schema.raw('DROP TYPE "product_availability"'),
+                    trx.schema.raw('DROP TYPE "product_age_group"'),
+                    trx.schema.raw('DROP TYPE "product_condition"'),
+                    trx.schema.raw('DROP TYPE "product_target_gender"')
+                ]);
             });
     });
 };
