d781f7ed228db55bf0fa840b9e65657a5054bd16
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-29 12:20:57 +0300

    M2-39 Add new fields to migration

--- migrations/20140926174643_image_additional_fields.js
+++ migrations/20140926174643_image_additional_fields.js
@@ -2,10 +2,17 @@
 
 exports.up = function (knex, Promise) {
     return knex.transaction(function (trx) {
-        return trx.schema
-            .table('image', function (table) {
-                table.specificType('main_color', 'CHAR(7)');
-            });
+        return Promise.all([
+            trx.schema.raw('CREATE TYPE "image_color_schema" AS ENUM (\'light\', \'dark\')')
+        ]).then(function () {
+            return trx.schema
+                .table('image', function (table) {
+                    table.specificType('main_color', 'CHAR(7)');
+                    table.specificType('color_schema', 'image_color_schema')
+                        .defaultTo('light')
+                        .notNullable();
+                });
+        });
     });
 };
 
@@ -14,6 +21,12 @@ exports.down = function (knex, Promise) {
         return trx.schema
             .table('image', function (table) {
                 table.dropColumn('main_color');
+                table.dropColumn('color_schema');
+            })
+            .then(function () {
+                return Promise.all([
+                    trx.schema.raw('DROP TYPE "image_color_schema"')
+                ]);
             });
     });
 };
