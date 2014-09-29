eaf58e1829f6f948a6c5ac7893759c67290f9833
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-29 17:23:21 +0300

    M2-44 Shop's PayPal account table

--- migrations/20140929144217_shop_paypal_account.js
+++ migrations/20140929144217_shop_paypal_account.js
@@ -3,35 +3,41 @@
 exports.up = function (knex, Promise) {
     return knex.transaction(function (trx) {
         var payPalPermissions = [
-            'EXPRESS_CHECKOUT',
-            'DIRECT_PAYMENT',
-            'SETTLEMENT_CONSOLIDATION',
-            'SETTLEMENT_REPORTING',
-            'AUTH_CAPTURE',
-            'MOBILE_CHECKOUT',
-            'BILLING_AGREEMENT',
-            'REFERENCE_TRANSACTION',
-            'AIR_TRAVEL',
-            'MASS_PAY',
-            'TRANSACTION_DETAILS',
-            'TRANSACTION_SEARCH',
-            'RECURRING_PAYMENTS',
-            'ACCOUNT_BALANCE',
-            'ENCRYPTED_WEBSITE_PAYMENTS',
-            'REFUND',
-            'NON_REFERENCED_CREDIT',
-            'BUTTON_MANAGER',
-            'MANAGE_PENDING_TRANSACTION_STATUS',
-            'RECURRING_PAYMENT_REPORT',
-            'EXTENDED_PRO_PROCESSING_REPORT',
-            'EXCEPTION_PROCESSING_REPORT',
-            'ACCOUNT_MANAGEMENT_PERMISSION',
-            'ACCESS_BASIC_PERSONAL_DATA',
-            'ACCESS_ADVANCED_PERSONAL_DATA',
-            'INVOICING'
-        ];
+                'EXPRESS_CHECKOUT',
+                'DIRECT_PAYMENT',
+                'SETTLEMENT_CONSOLIDATION',
+                'SETTLEMENT_REPORTING',
+                'AUTH_CAPTURE',
+                'MOBILE_CHECKOUT',
+                'BILLING_AGREEMENT',
+                'REFERENCE_TRANSACTION',
+                'AIR_TRAVEL',
+                'MASS_PAY',
+                'TRANSACTION_DETAILS',
+                'TRANSACTION_SEARCH',
+                'RECURRING_PAYMENTS',
+                'ACCOUNT_BALANCE',
+                'ENCRYPTED_WEBSITE_PAYMENTS',
+                'REFUND',
+                'NON_REFERENCED_CREDIT',
+                'BUTTON_MANAGER',
+                'MANAGE_PENDING_TRANSACTION_STATUS',
+                'RECURRING_PAYMENT_REPORT',
+                'EXTENDED_PRO_PROCESSING_REPORT',
+                'EXCEPTION_PROCESSING_REPORT',
+                'ACCOUNT_MANAGEMENT_PERMISSION',
+                'ACCESS_BASIC_PERSONAL_DATA',
+                'ACCESS_ADVANCED_PERSONAL_DATA',
+                'INVOICING'
+            ],
+            payPalAccountTypes = [
+                'PERSONAL',
+                'PREMIER',
+                'BUSINESS'
+            ];
         return Promise.all([
-            trx.schema.raw('CREATE TYPE "paypal_permission" AS ENUM (\'' + payPalPermissions.join('\', \'') + '\')')
+            trx.schema.raw('CREATE TYPE "paypal_account_permission" AS ENUM (\'' + payPalPermissions.join('\', \'') + '\')'),
+            trx.schema.raw('CREATE TYPE "paypal_account_type" AS ENUM (\'' + payPalAccountTypes.join('\', \'') + '\')')
         ]).then(function () {
             return trx.schema
                 .createTable('shop_paypal_account', function (table) {
@@ -42,9 +48,18 @@ exports.up = function (knex, Promise) {
                         .inTable('shop')
                         .onDelete('CASCADE')
                         .onUpdate('CASCADE');
-                    table.string('email')
+                    table.string('account_email')
                         .notNullable();
-                    table.specificType('permissions', 'paypal_permission[]');
+                    table.string('account_id')
+                        .notNullable();
+                    table.string('first_name')
+                        .notNullable();
+                    table.string('last_name')
+                        .notNullable();
+                    table.string('business_name')
+                        .notNullable();
+                    table.specificType('account_type', 'paypal_account_type');
+                    table.specificType('account_permissions', 'paypal_account_permission[]');
                     table.string('token')
                         .notNullable();
                     table.string('secret')
@@ -61,7 +76,8 @@ exports.down = function (knex, Promise) {
             .dropTable('shop_paypal_account')
             .then(function () {
                 return Promise.all([
-                    trx.schema.raw('DROP TYPE "paypal_permission"')
+                    trx.schema.raw('DROP TYPE "paypal_account_permission"'),
+                    trx.schema.raw('DROP TYPE "paypal_account_type"')
                 ]);
             });
     });
