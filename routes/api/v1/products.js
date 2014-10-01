ff6b2ab8371ba2f8eb1952c7f929362b0f1786ed
Dmitry Les <dmitriy.s.les@gmail.com>
2014-10-01 20:36:39 +0300

    M2-52 Implement new validation for all endpoints

--- routes/api/v1/products.js
+++ routes/api/v1/products.js
@@ -4,7 +4,7 @@ var express = require('express'),
     Bookshelf = require('../../../modules/bookshelf'),
     ProductsCollection = require('../../../collections/products'),
     ProductModel = require('../../../models/product'),
-    expressAsyncValidator = require('../../../modules/express-async-validator/module');
+    validator = require('../../../modules/express-async-validator/module');
 
 router.use('/', function (req, res, next) {
     res.set({
@@ -13,7 +13,7 @@ router.use('/', function (req, res, next) {
     next();
 });
 
-router.get('/', require('./middleware/collection_params_check'));
+router.get('/', validator(require('./validation/collection.json'), {source: 'query', param: 'collectionForm'}));
 
 router.get('/', function (req, res, next) {
     var productsCollection = new ProductsCollection();
@@ -22,8 +22,8 @@ router.get('/', function (req, res, next) {
     var collectionRequest = productsCollection
         .query(function (qb) {
             qb
-                .limit(req.query.limit)
-                .offset(req.query.offset);
+                .limit(req['collectionForm'].limit)
+                .offset(req['collectionForm'].offset);
         })
         .fetch({
             withRelated: ['shop.image']
@@ -51,86 +51,46 @@ router.get('/', function (req, res, next) {
 
 router.post('/', require('./middleware/auth_check'));
 
+router.post('/', validator(require('./validation/products/create.json'), {source: 'body', param: 'createForm'}));
+
 router.post('/', function (req, res, next) {
     new ProductModel()
-        .save(req.body, {req: req})
+        .save(req['createForm'])
         .then(function (productModel) {
-            new ProductModel({id: productModel.id})
-                .fetch({
-                    withRelated: ['shop.image']
-                })
-                .then(function (productModel) {
-                    res.set('Location', (req.secure ? 'https' : 'http') + '://' + req.get('host') + '/api/v1/products/' + productModel.id);
-                    res.status(201).json(productModel);
-                });
+            return productModel.load(['shop.image']);
         })
-        .catch(ProductModel.PermissionError, function (error) {
-            var forbiddenError = new (require('./errors/forbidden'))(error.message);
-            next(forbiddenError);
-        })
-        .catch(ProductModel.ValidationError, function (error) {
-            var validationError = new (require('./errors/validation'))("Validation failed", error);
-            next(validationError);
-        })
-        .catch(ProductModel.InternalServerError, function (error) {
-            var internalServerError = new (require('./errors/internal'))(error.message);
-            next(internalServerError);
-        })
-        .catch(function (error) {
-            var internalServerError = new (require('./errors/internal'))();
-            next(internalServerError);
+        .then(function (productModel) {
+            res.set('Location', (req.secure ? 'https' : 'http') + '://' + req.get('host') + '/api/v1/products/' + productModel.id);
+            res.status(201).json(productModel);
         });
 });
 
 router.use('/:productId', function (req, res, next) {
     res.set({
-        'Access-Control-Allow-Methods': 'GET,PUT,DELETE'
+        'Access-Control-Allow-Methods': 'GET,PUT'
     });
     next();
 });
 
 router.param('productId', function (req, res, next) {
-    req
-        .model({
-            "productId": {
-                "rules":      {
-                    "required":  {
-                        "message": "Product ID is required"
-                    },
-                    "isNumeric": {
-                        "message": "Product ID should be numeric"
-                    },
-                    "toInt":     {}
-                },
-                "source":     ["params"],
-                "allowEmpty": false
-            }
+    var productModel = new ProductModel({id: parseInt(req.params.productId)});
+    productModel.fetch({require: true})
+        .then(function (model) {
+            req.product = model;
+            next();
         })
-        .validate()
-        .then(function () {
-            var productModel = new ProductModel({id: req.params.productId});
-            productModel.fetch({require: true})
-                .then(function (model) {
-                    req.product = model;
-                    next();
-                })
-                .catch(ProductModel.NotFoundError, function () {
-                    var notFoundError = new (require('./errors/not_found'))("Product was not found");
-                    next(notFoundError);
-                })
-                .catch(function (err) {
-                    next(err);
-                });
+        .catch(ProductModel.NotFoundError, function () {
+            var notFoundError = new (require('./errors/not_found'))("Product was not found");
+            next(notFoundError);
         })
-        .catch(function (error) {
-            var badRequestError = new (require('./errors/bad_request'))("Bad request", error);
-            next(badRequestError);
+        .catch(function (err) {
+            next(err);
         });
 });
 
 router.get('/:productId', function (req, res) {
     req.product
-        .load('shop.image')
+        .load(['shop.image'])
         .then(function () {
             res.json(req.product);
         });
@@ -138,33 +98,16 @@ router.get('/:productId', function (req, res) {
 
 router.put('/:productId', require('./middleware/auth_check'));
 
+router.put('/:productId', validator(require('./validation/products/update.json'), {source: 'body', param: 'updateForm'}));
+
 router.put('/:productId', function (req, res, next) {
     req.product
-        .save(req.body, {req: req})
+        .save(req['updateForm'])
         .then(function (productModel) {
-            new ProductModel({id: productModel.id})
-                .fetch({
-                    withRelated: ['shop.image']
-                })
-                .then(function (productModel) {
-                    res.status(200).json(productModel);
-                });
+            return productModel.load(['shop.image']);
         })
-        .catch(ProductModel.PermissionError, function (error) {
-            var forbiddenError = new (require('./errors/forbidden'))(error.message);
-            next(forbiddenError);
-        })
-        .catch(ProductModel.ValidationError, function (error) {
-            var validationError = new (require('./errors/validation'))("Validation failed", error);
-            next(validationError);
-        })
-        .catch(ProductModel.InternalServerError, function (error) {
-            var internalServerError = new (require('./errors/internal'))(error.message);
-            next(internalServerError);
-        })
-        .catch(function (error) {
-            var internalServerError = new (require('./errors/internal'))();
-            next(internalServerError);
+        .then(function (productModel) {
+            res.status(200).json(productModel);
         });
 });
 
