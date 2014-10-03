990a73e377d3b90aba4d549edd52fe82f18ef9a0
Dmitry Les <dmitriy.s.les@gmail.com>
2014-10-03 20:04:36 +0300

    M2-54 Order flow

--- routes/api/v1/orders.js
+++ routes/api/v1/orders.js
@@ -43,7 +43,7 @@ router.post('/ipn', function (req, res, next) {
     console.info(req.headers);
 
     request({
-        url:    'http://sandbox.paypal.com/ipn',
+        url:    'https://sandbox.paypal.com/cgi-bin/webscr',
         method: 'POST',
         form:   _.extend({'cmd': '_notify-validate'}, ipnMessage)
     }, function (error, response, body) {
