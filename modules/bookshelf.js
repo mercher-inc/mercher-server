139a43e0f27bfce67b9fb4c747bf474d2334fd1f
Dmitry Les <dmitriy.s.les@gmail.com>
2014-10-10 13:19:01 +0300

    M2-54 Order flow

--- modules/bookshelf.js
+++ modules/bookshelf.js
@@ -1,11 +1,11 @@
 var knex = require('knex')({
         client:     'pg',
         connection: {
-            host:     process.env.RDS_HOSTNAME,
-            user:     process.env.RDS_USERNAME,
-            password: process.env.RDS_PASSWORD,
-            port:     process.env.RDS_PORT,
-            database: process.env.RDS_DB_NAME,
+            host:     process.env['RDS_HOSTNAME'] || 'localhost',
+            user:     process.env['RDS_USERNAME'] || 'postgres',
+            password: process.env['RDS_PASSWORD'] || 'postgres',
+            port:     process.env['RDS_PORT']     || '5432',
+            database: process.env['RDS_DB_NAME']  || 'mercher',
             charset:  'utf8'
         },
         migrations: {
